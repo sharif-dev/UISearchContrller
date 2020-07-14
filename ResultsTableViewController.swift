5ee36597af8dfd79338829d9abc85007c2de59ec
mohsen <mohsen@mohsens-Mac.local>
2020-07-14 15:46:47 -0700

    Adding Search Tokens

--- ResultsTableViewController.swift
+++ ResultsTableViewController.swift
@@ -38,21 +38,79 @@ class ResultsTableViewController: UITableViewController {
       tableView.reloadData()
     }
   }
+    
+  var searchTokens: [UISearchToken] = []
+    
+    var isFilteringByCountry: Bool {
+      return countries != nil
+    }
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    makeTokens()
   }
   
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return countries?.count ?? 0
-  }
+//  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//    return countries?.count ?? 0
+//  }
   
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    if
-      let cell = tableView.dequeueReusableCell(withIdentifier: "results", for: indexPath) as? CountryCell {
-      cell.country = countries?[indexPath.row]
-      return cell
+    override func tableView(
+      _ tableView: UITableView,
+      numberOfRowsInSection section: Int
+    ) -> Int {
+      return isFilteringByCountry ? (countries?.count ?? 0) : searchTokens.count
     }
-    return UITableViewCell()
+
+    
+//  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//    if
+//      let cell = tableView.dequeueReusableCell(withIdentifier: "results", for: indexPath) as? CountryCell {
+//      cell.country = countries?[indexPath.row]
+//      return cell
+//    }
+//    return UITableViewCell()
+//  }
+    override func tableView(
+      _ tableView: UITableView,
+      cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+      // 1
+      if
+        isFilteringByCountry,
+        let cell = tableView.dequeueReusableCell(
+          withIdentifier: "results",
+          for: indexPath) as? CountryCell {
+        cell.country = countries?[indexPath.row]
+        return cell
+      
+      // 2
+      } else if
+        let cell = tableView.dequeueReusableCell(
+          withIdentifier: "search",
+          for: indexPath) as? SearchTokenCell {
+        cell.token = searchTokens[indexPath.row]
+        return cell
+      }
+
+      // 3
+      return UITableViewCell()
+    }
+}
+
+// MARK: -
+
+extension ResultsTableViewController {
+  func makeTokens() {
+    // 1
+    let continents = Continent.allCases
+    searchTokens = continents.map { (continent) -> UISearchToken in
+      // 2
+      let globeImage = UIImage(systemName: "globe")
+      let token = UISearchToken(icon: globeImage, text: continent.description)
+      // 3
+      token.representedObject = Continent(rawValue: continent.description)
+      // 4
+      return token
+    }
   }
 }
